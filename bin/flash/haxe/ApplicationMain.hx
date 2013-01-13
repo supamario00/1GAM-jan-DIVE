3fe817e17aa722ff8faa86fdb7d13cc2e045417e
William Dula <dize01@gmail.com>
2013-01-13 02:34:15 -0600

    build 1

---
+++ bin/flash/haxe/ApplicationMain.hx
@@ -0,0 +1,280 @@
+#if nme
+
+import Main;
+import nme.Assets;
+import nme.events.Event;
+
+
+class ApplicationMain {
+	
+	static var mPreloader:NMEPreloader;
+
+	public static function main () {
+		
+		var call_real = true;
+		
+		
+		var loaded:Int = nme.Lib.current.loaderInfo.bytesLoaded;
+		var total:Int = nme.Lib.current.loaderInfo.bytesTotal;
+		
+		nme.Lib.current.stage.align = nme.display.StageAlign.TOP_LEFT;
+		nme.Lib.current.stage.scaleMode = nme.display.StageScaleMode.NO_SCALE;
+		
+		if (loaded < total || true) /* Always wait for event */ {
+			
+			call_real = false;
+			mPreloader = new NMEPreloader();
+			nme.Lib.current.addChild(mPreloader);
+			mPreloader.onInit();
+			mPreloader.onUpdate(loaded,total);
+			nme.Lib.current.addEventListener (nme.events.Event.ENTER_FRAME, onEnter);
+			
+		}
+		
+		
+		
+		
+		
+		
+
+		if (call_real)
+			begin ();
+	}
+
+	
+	
+	private static function begin () {
+		
+		var hasMain = false;
+		
+		for (methodName in Type.getClassFields(Main))
+		{
+			if (methodName == "main")
+			{
+				hasMain = true;
+				break;
+			}
+		}
+		
+		if (hasMain)
+		{
+			Reflect.callMethod (Main, Reflect.field (Main, "main"), []);
+		}
+		else
+		{
+			var instance = Type.createInstance(Main, []);
+			if (Std.is (instance, nme.display.DisplayObject)) {
+				nme.Lib.current.addChild(cast instance);
+			}	
+		}
+		
+	}
+
+	static function onEnter (_) {
+		
+		var loaded = nme.Lib.current.loaderInfo.bytesLoaded;
+		var total = nme.Lib.current.loaderInfo.bytesTotal;
+		mPreloader.onUpdate(loaded,total);
+		
+		if (loaded >= total) {
+			
+			nme.Lib.current.removeEventListener(nme.events.Event.ENTER_FRAME, onEnter);
+			mPreloader.addEventListener (Event.COMPLETE, preloader_onComplete);
+			mPreloader.onLoaded();
+			
+		}
+		
+	}
+
+	public static function getAsset (inName:String):Dynamic {
+		
+		
+		if (inName=="assets/data/autotiles.png")
+			 
+            return Assets.getBitmapData ("assets/data/autotiles.png");
+         
+		
+		if (inName=="assets/data/autotiles_alt.png")
+			 
+            return Assets.getBitmapData ("assets/data/autotiles_alt.png");
+         
+		
+		if (inName=="assets/data/beep.mp3")
+			 
+            return Assets.getSound ("assets/data/beep.mp3");
+		 
+		
+		if (inName=="assets/data/button.png")
+			 
+            return Assets.getBitmapData ("assets/data/button.png");
+         
+		
+		if (inName=="assets/data/courier.ttf")
+			 
+			 return Assets.getFont ("assets/data/courier.ttf");
+		 
+		
+		if (inName=="assets/data/cursor.png")
+			 
+            return Assets.getBitmapData ("assets/data/cursor.png");
+         
+		
+		if (inName=="assets/data/default.png")
+			 
+            return Assets.getBitmapData ("assets/data/default.png");
+         
+		
+		if (inName=="assets/data/handle.png")
+			 
+            return Assets.getBitmapData ("assets/data/handle.png");
+         
+		
+		if (inName=="assets/data/logo.png")
+			 
+            return Assets.getBitmapData ("assets/data/logo.png");
+         
+		
+		if (inName=="assets/data/logo_corners.png")
+			 
+            return Assets.getBitmapData ("assets/data/logo_corners.png");
+         
+		
+		if (inName=="assets/data/logo_light.png")
+			 
+            return Assets.getBitmapData ("assets/data/logo_light.png");
+         
+		
+		if (inName=="assets/data/nokiafc22.ttf")
+			 
+			 return Assets.getFont ("assets/data/nokiafc22.ttf");
+		 
+		
+		if (inName=="assets/data/vcr/flixel.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/flixel.png");
+         
+		
+		if (inName=="assets/data/vcr/open.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/open.png");
+         
+		
+		if (inName=="assets/data/vcr/pause.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/pause.png");
+         
+		
+		if (inName=="assets/data/vcr/play.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/play.png");
+         
+		
+		if (inName=="assets/data/vcr/record_off.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/record_off.png");
+         
+		
+		if (inName=="assets/data/vcr/record_on.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/record_on.png");
+         
+		
+		if (inName=="assets/data/vcr/restart.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/restart.png");
+         
+		
+		if (inName=="assets/data/vcr/step.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/step.png");
+         
+		
+		if (inName=="assets/data/vcr/stop.png")
+			 
+            return Assets.getBitmapData ("assets/data/vcr/stop.png");
+         
+		
+		if (inName=="assets/data/vis/bounds.png")
+			 
+            return Assets.getBitmapData ("assets/data/vis/bounds.png");
+         
+		
+		
+		return null;
+		
+	}
+	
+	
+	private static function preloader_onComplete (event:Event):Void {
+		
+		mPreloader.removeEventListener (Event.COMPLETE, preloader_onComplete);
+		
+		nme.Lib.current.removeChild(mPreloader);
+		mPreloader = null;
+		
+		begin ();
+		
+	}
+	
+}
+
+class NME_assets_data_autotiles_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_autotiles_alt_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_beep_mp3 extends nme.media.Sound { }
+class NME_assets_data_button_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_courier_ttf extends nme.text.Font { }
+class NME_assets_data_cursor_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_default_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_handle_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_logo_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_logo_corners_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_logo_light_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_nokiafc22_ttf extends nme.text.Font { }
+class NME_assets_data_vcr_flixel_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_open_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_pause_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_play_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_record_off_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_record_on_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_restart_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_step_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vcr_stop_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+class NME_assets_data_vis_bounds_png extends nme.display.BitmapData { public function new () { super (0, 0); } }
+
+
+#else
+
+import Main;
+
+class ApplicationMain {
+	
+	public static function main () {
+		
+		var hasMain = false;
+		
+		for (methodName in Type.getClassFields(Main))
+		{
+			if (methodName == "main")
+			{
+				hasMain = true;
+				break;
+			}
+		}
+		
+		if (hasMain)
+		{
+			Reflect.callMethod (Main, Reflect.field (Main, "main"), []);
+		}
+		else
+		{
+			var instance = Type.createInstance(Main, []);
+			if (Std.is (instance, flash.display.DisplayObject)) {
+				flash.Lib.current.addChild(cast instance);
+			}
+		}
+		
+	}
+
+}
+
+#end
